caeea2c2fc16c15c25d077c246f73783dc51cc71
Piyush Khanna <khanna.piyush30@gmail.com>
2019-09-14 19:27:09 +0530

    Added gui using tkinter

--- virtual_assistant.py
+++ virtual_assistant.py
@@ -1,16 +1,54 @@
+import tkinter
 import wikipedia
 import wolframalpha
-while (True):
-	user_input = input("Question: ")
-	try:
-		try:
-			app_id = "******-**********"            #Your app id
-			client = wolframalpha.Client(app_id)
-			res = client.query(user_input)
-			answere = next(res.results).text
-			print(answere)
-		except:	
-			answere = wikipedia.summary(user_input)
-			print(answere)
-	except:
-		print("Some error encountered")
+
+def result_window(event=None):
+    def destroy_result_window(event=None):
+        query_result_window.destroy()
+    
+    query_result_window=tkinter.Toplevel()
+    query_result_window.minsize(400,400)
+    query_result_window.maxsize(400,400)
+    tkinter.Label(query_result_window,bg="#95a5a6",height=400,width=400).place(x=0,y=0)
+    result = tkinter.Text(query_result_window,height=18,width=47,bg="#d0d3d4")
+    result.place(x=10,y=40)
+    user_input=query_input.get()
+    try:
+        try:
+            app_id = "******-**********"            #Your app id
+            client = wolframalpha.Client(app_id)
+            res = client.query(user_input)
+            answere = next(res.results).text
+            clear_text()
+        except: 
+            answere = wikipedia.summary(user_input,sentences=2)
+            clear_text()
+    except:
+        answere="""Some error occured. 
+        >Check if you have filled the field.
+        >Try to use less and meaningful words."""
+
+    quote = answere 
+    result.insert(tkinter.END, quote)
+
+    tkinter.Button(query_result_window,text="Close",command=query_result_window.destroy,font=("Comic Sans MS", 10), width = 8,bg="#808b96").place(x=310,y=350)
+    query_result_window.bind('<Escape>', destroy_result_window)
+def destroy_root(event=None):
+    root.destroy()
+def clear_text():
+    query_input.delete(0,'end')
+
+root = tkinter.Tk()
+root.title("Virtual Assistant")
+root.minsize(400,400)
+root.maxsize(400,400)
+root.geometry("400x400")
+query_label=tkinter.Label(root, text="Hey! How may I help you?",font=("Comic Sans MS", 12),bg="#d0d3d4")
+query_label.place(x=0,y=100)
+query_input=tkinter.Entry(root,font=("Comic Sans MS", 12),bg="#d0d3d4")
+query_input.place(x=200,y=135)
+tkinter.Button(root,text="Clear",command=clear_text,font=("Comic Sans MS", 10), width = 8,bg="#808b96").place(x=90,y=350)
+tkinter.Button(root,text="Search",command=result_window,font=("Comic Sans MS", 10), width = 8,bg="#808b96").place(x=230,y=350)
+root.bind('<Return>', result_window)
+root.bind('<Escape>', destroy_root)
+root.mainloop()
